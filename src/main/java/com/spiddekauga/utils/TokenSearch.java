318265573ca6da33a6a1d1360b5ef6731b1b3508
senth <senth.wallace@gmail.com>
2016-07-12 12:30:04 +0200

    Change: Improve sort in TokenSearch — sorts first by starting of word if uses ALL

--- src/main/java/com/spiddekauga/utils/TokenSearch.java
+++ src/main/java/com/spiddekauga/utils/TokenSearch.java
@@ -15,23 +15,24 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * A class that helps one search for objects with auto-complete functionality. I.e. it
- * creates tokens that can be searched. Not the most efficient way, but OK at the moment.
+ * A class that helps one search for objects with auto-complete functionality. I.e. it creates
+ * tokens that can be searched. Not the most efficient way, but OK at the moment.
  * @param <Searchable> The object type that is stored and searchable
  */
 public class TokenSearch<Searchable> {
 private Set<Searchable> mObjects = new HashSet<>();
 private Multimap<String, Searchable> mTokenObjects = ArrayListMultimap.create();
 private Multimap<Searchable, String> mObjectTokens = ArrayListMultimap.create();
+private Multimap<Searchable, String> mFullWords = ArrayListMultimap.create();
 
 /**
- * Update an object's search tokens. This will remove all previous search tokens from
- * this object. I.e. this is a shortcut method for first calling
- * {@link #remove(Object)} and then {@link #add(Object, TokenizePatterns, String...)}
+ * Update an object's search tokens. This will remove all previous search tokens from this object.
+ * I.e. this is a shortcut method for first calling {@link #remove(Object)} and then {@link
+ * #add(Object, TokenizePatterns, String...)}
  * @param object the object that should be found if these are auto-completed.
  * @param tokenizePattern how the words should be tokenized
- * @param texts the words that should be auto-completed. The words will be tokenized
- * according to the tokenize pattern specified
+ * @param texts the words that should be auto-completed. The words will be tokenized according to
+ * the tokenize pattern specified
  */
 public void update(Searchable object, TokenizePatterns tokenizePattern, String... texts) {
 	remove(object);
@@ -50,14 +51,14 @@ public void remove(Searchable object) {
 			mTokenObjects.remove(token, object);
 		}
 	}
-	}
+}
 
 /**
  * Add a search token to the object that should be searchable
  * @param object the object that should be found if these are auto-completed.
  * @param tokenizePattern how the words should be tokenized
- * @param texts the words that should be auto-completed. The words will be tokenized
- * according to the tokenize pattern specified
+ * @param texts the words that should be auto-completed. The words will be tokenized according to
+ * the tokenize pattern specified
  */
 public void add(Searchable object, TokenizePatterns tokenizePattern, String... texts) {
 	if (!mObjects.contains(object)) {
@@ -70,16 +71,23 @@ public void add(Searchable object, TokenizePatterns tokenizePattern, String... t
 		for (String token : tokens) {
 			mTokenObjects.put(token, object);
 			mObjectTokens.put(object, token);
+
+		}
+
+		// When we tokenize every part of the word we want to give search strings that
+		// are in the beginning of a word higher relevance
+		if (tokenizePattern == TokenizePatterns.ALL) {
+			List<String> words = Strings.tokenize(TokenizePatterns.WORD, text.toLowerCase());
+			mFullWords.putAll(object, words);
 		}
 	}
 }
 
 /**
  * Search for objects. Case insensitive
- * @param searchString when searching for more than two words AND both words has to be
- * found for the object. If this is empty every object is returned
- * @return found objects sorted by relevance. This list is just a copy and is always
- * OK to change
+ * @param searchString when searching for more than two words AND both words has to be found for the
+ * object. If this is empty every object is returned
+ * @return found objects sorted by relevance. This list is just a copy and is always OK to change
  */
 public List<Searchable> search(CharSequence searchString) {
 	return search((String) searchString);
@@ -87,13 +95,13 @@ public List<Searchable> search(CharSequence searchString) {
 
 /**
  * Search for objects. Case insensitive
- * @param searchString when searching for more than two words AND both words has to be
- * found for the object. If this is empty every object is returned
- * @return found objects sorted by relevance. This list is just a copy and is always
- * OK to change
+ * @param searchString when searching for more than two words AND both words has to be found for the
+ * object. If this is empty every object is returned
+ * @return found objects sorted by relevance. This list is just a copy and is always OK to change
  */
 public List<Searchable> search(String searchString) {
-	if (searchString.trim().isEmpty()) {
+	searchString = searchString.trim().toLowerCase();
+	if (searchString.isEmpty()) {
 		return new ArrayList<>(mObjects);
 	}
 
@@ -102,7 +110,7 @@ public List<Searchable> search(String searchString) {
 	int tokenCount = 0;
 
 	// Find
-	String[] searchWords = searchString.trim().toLowerCase().split(" ");
+	String[] searchWords = searchString.split(" ");
 	for (String token : searchWords) {
 		if (!token.isEmpty()) {
 			tokenCount++;
@@ -112,11 +120,12 @@ public List<Searchable> search(String searchString) {
 			for (Searchable foundObject : foundByToken) {
 				AtomicInteger relevanceCount = relevanceCounts.get(foundObject);
 
+				int relevance = getRelevance(foundObject, searchString);
 				if (relevanceCount == null) {
-					relevanceCount = new AtomicInteger(1);
+					relevanceCount = new AtomicInteger(relevance);
 					relevanceCounts.put(foundObject, relevanceCount);
 				} else {
-					relevanceCount.incrementAndGet();
+					relevanceCount.addAndGet(relevance);
 				}
 
 				Set<String> usesToken = usesTokens.get(foundObject);
@@ -147,6 +156,32 @@ public List<Searchable> search(String searchString) {
 	});
 
 	return foundAndSorted;
+}
+
+/**
+ * Get the relevance for the object. Checks for all whole words of the object and adds 1 for each
+ * word the searchString matches (at the start), and 2 if the searchString matches the whole word.
+ * @param object object we want to get the relevance for.
+ * @param searchString what the user search for
+ * @return 1 + 1 for matching start of a word for the object
+ */
+private int getRelevance(Searchable object, String searchString) {
+	int relevance = 1;
+
+	Collection<String> words = mFullWords.get(object);
+	if (words != null) {
+		for (String word : words) {
+			if (word.startsWith(searchString)) {
+				relevance += 1;
+
+				if (word.equals(searchString)) {
+					relevance += 1;
+				}
+			}
+		}
+	}
+
+	return relevance;
 }
 
 }
