99856f088c30f0ecee1185c5e83bc05ce1edde0c
senth <senth.wallace@gmail.com>
2017-01-20 14:41:21 +0100

    Add default ISO time formatter

--- src/main/java/com/spiddekauga/utils/Time.java
+++ src/main/java/com/spiddekauga/utils/Time.java
@@ -1,14 +1,27 @@
 package com.spiddekauga.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 /**
  * Various time methods
  */
 public class Time {
+/** ISO Date */
+public static final String ISO_DATE = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
+
 /**
- * Causes the currently executing thread to sleep (temporarily cease
- * execution) for the specified number of milliseconds, subject to
- * the precision and accuracy of system timers and schedulers. The thread
- * does not lose ownership of any monitors.
+ * Create a simple date format from the ISO date
+ * @return simple date format with ISO date
+ */
+public static SimpleDateFormat createIsoDateFormat() {
+	return new SimpleDateFormat(ISO_DATE, Locale.ENGLISH);
+}
+
+/**
+ * Causes the currently executing thread to sleep (temporarily cease execution) for the specified
+ * number of milliseconds, subject to the precision and accuracy of system timers and schedulers.
+ * The thread does not lose ownership of any monitors.
  * @param millis the length of time to sleep in milliseconds
  * @throws IllegalArgumentException if the value of {@code millis} is negative
  */
